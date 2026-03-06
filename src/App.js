513efe5d1449e7a5ffce58ea41bc3e226cd3b53c
aadesope30 <adesope.ajayi@ucalgary.ca>
2026-03-06 12:00:16 -0700

    linked locale repo

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const currentYear = new Date().getFullYear();
+  let isLoggedIn = false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>Curret-Year is {currentYear}</p>
+      {isLoggedIn? "welcome back" : "Please log in"}
     </div>
   );
 }
